ac882a13993c67bd6e72246e1e4f61e96342e3d2
dung2502 <nguyenducdung704@gmail.com>
2023-10-04 21:03:41 +0700

    Extend

---
+++ Module2/Code_Module2/src/Extend/Main.java
@@ -0,0 +1,33 @@
+package Extend;
+
+ class test extends father_Test {
+     protected void method(){}
+     void method1(){}
+     public void me(){}
+     public void speak(){
+        System.out.println("speak");
+        super.speak();
+    }
+    public void train(){
+        this.speak();
+    }
+}
+ class father_Test{
+     public void me(){}
+    public  void speak(){
+        System.out.println("speak son");
+    }
+     public father_Test() {
+         System.out.println("Here");
+     }
+ }
+ class test2 extends father_Test{}
+ public class Main{
+     public static void main(String[] args) {
+         father_Test b = new test2();
+         father_Test[] c = new father_Test[5];
+        test a = new test();
+        a.speak();
+        a.train();
+     }
+ }
